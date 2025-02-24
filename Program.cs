a2bc2b76e9686a06d5f344c7024bee523ce2199d
Erik07317 <eriksabino2030@gmail.com>
2025-02-24 19:54:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+Console.WriteLine("Frutas Preferidas"); 
+Console.WriteLine();
+
+Console.WriteLine("1. ");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Banana");
+Console.ResetColor();
+
+Console.WriteLine("2. ");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Maçã verde");
+Console.ResetColor();
+
+Console.WriteLine("3. ");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Morango");
+Console.ResetColor();
+
+Console.WriteLine("4. ");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Uva");
+Console.ResetColor();
+
+Console.WriteLine("5. ");
+
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("Kiwi");
+Console.ResetColor();
+
+
+
